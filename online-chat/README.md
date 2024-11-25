3bff1188dce51fe8bd7622baf742e7ae31754936
Purya-s <156438302+Purya-s@users.noreply.github.com>
2024-11-26 00:29:36 +0330

    Add files via upload

---
+++ online-chat/README.md
@@ -0,0 +1,11 @@
+# Jatte
+
+This project is the base starter for a tutorial from Code With Stein
+
+
+cheatsheet::
+    cd jatte-starter
+
+    npx tailwindcss -i ./static/css/main.css -o ./static/css/main.min.css --watch
+    
+    python manage.py runserver
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*/templates/*/*.html', 
+    './*/templates/*/*/*.html', 
+    './*/*.py',
+    '../../**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
